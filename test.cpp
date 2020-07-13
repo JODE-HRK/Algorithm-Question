4a7fe6e61d11fa01b6fad378a8add0cb60f58fbc
JODE-HRK <201830081093@smail.hunnu.edu.cn>
2020-07-13 16:56:11 +0800

    2020.7.13

--- test.cpp
+++ test.cpp
@@ -1,96 +1,51 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int maxn = 1e6 + 7;
-int n, m, st, en;
-struct Edge
+int t, n, x;
+int g[201][201];
+int used[201], col[201];
+bool found(int x)
 {
-    int to, nxt;
-    ll w;
-} edge[maxn << 1];
-int head[maxn], tot = 0;
-int dis[maxn];
-void addEdge(int s, int c, int w)
-{
-    edge[tot].to = c, edge[tot].w = w, edge[tot].nxt = head[s];
-    head[s] = tot++;
-}
-int cur[maxn << 1];
-void init()
-{
-    for (int i = 0; i <= tot; i++)
-        cur[i] = head[i];
-}
-bool bfs()
-{
-    fill(dis, dis + 1 + n, -1);
-    queue<int> Q;
-    while (!Q.empty())
-        Q.pop();
-    Q.push(st);
-    dis[st] = 0;
-    while (!Q.empty())
+    for (int i = 1; i <= n; i++)
     {
-        int now = Q.front();
-        Q.pop();
-        for (int i = head[now]; ~i; i = edge[i].nxt)
+        if (g[x][i] && !used[i])
         {
-            int v = edge[i].to;
-            if (edge[i].w && dis[v] == -1)
+            used[i] = 1;
+            if (!col[i] || found(col[i]))//如果我要匹配的被匹配了，就再找上一个匹配到它的还有没有其他选择
             {
-                dis[v] = dis[now] + 1;
-                if (v == en)
-                    return 1;
-                Q.push(v);
+                col[i] = x; //记录为我当前匹配的
+                return 1;
             }
         }
     }
     return 0;
 }
-ll dfs(int s, ll flow)
-{
-    if (s == en)
-        return flow;
-    ll _flow = 0, __flow;
-    for (int i = cur[s]; ~i; i = edge[i].nxt)
-    {
-        int v = edge[i].to;
-        if (dis[v] == dis[s] + 1 && edge[i].w)
-        {
-            __flow = dfs(v, min(flow, edge[i].w));
-            flow -= __flow;
-            edge[i].w -= __flow;
-            _flow += __flow;
-            edge[i ^ 1].w += __flow;
-            if (!flow)
-                break;
-        }
-    }
-    if (!_flow)
-        dis[s] = -1;
-    return _flow;
-}
-ll Dinic()
-{
-    ll ans = 0;
-    while (bfs())
-    { //bfs构建层次网络 判断是否存在一条到en的点
-        init();
-        ans += dfs(st, 0x7fffffff); //一次dfs完成所有增广
-    }
-    return ans;
-}
 int main()
 {
-    scanf("%d %d %d %d", &n, &m, &st, &en);
-    fill(head, head + 2 + 2 * m, -1);
-    for (int i = 1; i <= m; i++)
+    cin >> t;
+    while (t--)
     {
-        int fr, to, w;
-        scanf("%d %d %d", &fr, &to, &w);
-        addEdge(fr, to, w);
-        addEdge(to, fr, 0);
+        scanf("%d", &n);
+        memset(g, 0, sizeof(g));
+        fill(col, col + (1 + n), 0);
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                scanf("%d", &x);
+                if (x)
+                    g[i][j] = 1;
+            }
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            fill(used, used + 1 + n, 0);
+            if (found(i))
+                sum++;
+        }
+        if (sum == n)
+            printf("Yes\n");
+        else
+            printf("No\n");
     }
-    printf("%lld", Dinic());
     return 0;
 }
