a1b079ee470ad042976979bf8a1e28432f90e5cf
JODE-HRK <201830081093@smail.hunnu.edu.cn>
2020-08-01 22:49:41 +0800

    2020.8.1

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
  * @Author: JODEHRK
  * @Date: 2020-07-13 21:35:45
  * @LastEditors: JODEHRK
- * @LastEditTime: 2020-07-18 15:20:21
+ * @LastEditTime: 2020-07-30 21:02:15
  */
 #include <bits/stdc++.h>
 #define ll long long
@@ -24,75 +24,21 @@ inline int read()
 		x = x * 10 + c - '0', c = getchar();
 	return x;
 }
-int n, m;
-int head[maxn], tot, fa[maxn];
-queue<int> Q[maxn];
-struct Edge
+int n;
+int a[maxn];
+struct Node
 {
-	int fr, to, nxt;
-} edge[maxn];
-void init()
+	int s, id;
+} b[maxn];
+bool cmp(Node x, Node y)
 {
-	tot = 0;
-	fill(head, head + 1 + n, -1);
-	for (int i = 0; i < n; i++)
-		fa[i] = i, Q[i].push(i);
-}
-void addEdge(int x, int y)
-{
-	edge[tot] = (Edge){x, y, head[x]};
-	head[x] = tot++;
-}
-int fdfa(int x)
-{
-	return fa[x] == x ? x : fa[x] = fdfa(fa[x]);
-}
-void add(int now)
-{
-	for (int i = head[now]; ~i; i = edge[i].nxt)
-	{
-		int tto = edge[i].to;
-		fa[fdfa(tto)] = now;
-		Q[now].push(tto);
-	}
+	return x.s > y.s;
 }
 int main()
 {
-	// freopen(".in", "r", stdin);
-	int t;
-	t = read();
-	while (t--)
-	{
-		n = read(), m = read();
-		init();
-		for (int i = 1; i <= m; i++)
-		{
-			int x, y;
-			x = read(), y = read();
-			addEdge(x, y);
-			addEdge(y, x);
-		}
-		int q;
-		q = read();
-		while (q--)
-		{
-			int o;
-			o = read();
-			int f = fdfa(o);
-			int s = Q[f].size();
-			while (s--)
-			{
-				int x = Q[f].front();
-				Q[f].pop();
-				add(f);
-			}
-		}
-		for (int i = 0; i < n; i++)
-			printf("%d ", fdfa(i));
-		cout << endl;
-	}
+	n = read();
+	for (int i = 1; i <= n; i++)
+		a[i] = read(), b[i].id = i, b[i].id = a[i];
+	sort(b + 1, b + 1 + n, cmp);
 	return 0;
 }
-/*
-
-*/
