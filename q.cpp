74c66ff6cc0e240f68f5a95a9d063118fd9f52b4
JODE-HRK <201830081093@smail.hunnu.edu.cn>
2020-09-20 16:14:24 +0800

    2020.9.13

--- q.cpp
+++ q.cpp
@@ -1,34 +1,16 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-const int maxn = 4e4 + 7;
-int n, m;
-int w[maxn], val[maxn];
-int dp[maxn];
-int ans[maxn];
-int getans(int l, int r, int maxx)
-{
-    fill(dp, dp + 1 + maxx, 0);
-    for (int i = l; i <= r; i++)
-        for (int j = maxx; j >= w[i]; j--)
-            dp[j] = max(dp[j], dp[j - w[i]] + val[i]);
-    return dp[maxx];
-} //01背包一定倒序
+ll n, mod;
+
 int main()
 {
-    scanf("%d %d", &n, &m);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &w[i]);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &val[i]);
-    for (int i = 1; i <= m; i++)
-    {
-        int l, r, t;
-        scanf("%d %d %d", &l, &r, &t);
-        ans[i] = getans(l, r, t);
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        printf("%d\n", ans[i]);
-    }
+    
     return 0;
 }
+/*
+3
+3 998244353
+100 998244353
+1000000000 998244353
+*/
